02a137106cef3609cc0ec55c1c280cac456bff0d
robysondepaula <robysonwillyandepaula@gmail.com>
2022-07-12 18:43:38 -0400

    Created tables, methods inside each class and done the second migration

---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus: int
+    {   
+        Pending = 0, 
+        Billed = 1, 
+        Cancelled = 2
+    }
+
+
+
+}
