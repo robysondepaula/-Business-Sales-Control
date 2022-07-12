ccf40657018726aa3aff0f1b75acd0119549c67c
robysondepaula <robysonwillyandepaula@gmail.com>
2022-07-12 14:29:01 -0400

    Changing some first attributes

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App.";
+            ViewData["Manager"] = "Robyson De Paula";
+            ViewData["Email"] = "robysondepaula.ca@gmail.com";
 
             return View();
         }
