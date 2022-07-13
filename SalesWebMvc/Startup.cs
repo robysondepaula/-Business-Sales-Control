e30fb09604f36fd779d0f8a4c6eb051caf7ac098
robysondepaula <robysonwillyandepaula@gmail.com>
2022-07-13 14:20:22 -0400

    Update values to dispaly using Annotations dipaly and display format

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace SalesWebMvc
 {
@@ -50,6 +52,7 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+          
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
