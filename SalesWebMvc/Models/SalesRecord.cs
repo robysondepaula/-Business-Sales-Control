8f4aeb36384f2e976865f9428356a9c8ebad30e2
robysondepaula <robysonwillyandepaula@gmail.com>
2022-07-13 17:29:57 -0400

    Implementing grouping search, finalazing the application

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public  SaleStatus Status { get; set; }
