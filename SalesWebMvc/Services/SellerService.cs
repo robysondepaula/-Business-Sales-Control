f04c756a21d28266827fd918b807c074e5c6f5e1
robysondepaula <robysonwillyandepaula@gmail.com>
2022-07-13 11:19:37 -0400

    Create Seller View Index and Create, Seller Controller Create and Post Methods

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,5 +21,10 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }//at the moment this is syncrhonic
 
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
