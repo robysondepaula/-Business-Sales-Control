3761816ce15070760b4e708215b763534085150a
robysondepaula <robysonwillyandepaula@gmail.com>
2022-07-13 15:59:47 -0400

    Exception handling for delete referencial integrity

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,7 +19,7 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public async Task< List<Seller>> FindAllAsync()
+        public async Task<List<Seller>> FindAllAsync()
         {
             return await _context.Seller.ToListAsync();
         }
@@ -30,36 +30,44 @@ namespace SalesWebMvc.Services
             await _context.SaveChangesAsync();
         }
 
-        public async Task <Seller> FindByIdAsync(int id)
+        public async Task<Seller> FindByIdAsync(int id)
         {
             return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
         }
-      
+
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
+
         }
 
         public async Task UpdateAsync(Seller obj)
         {
             bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
-            if (! hasAny)
+            if (!hasAny)
             {
                 throw new NotFoundException("Id not found!!");
             }
             try
             {
                 _context.Update(obj);
-               await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
             }
-            catch(DbUpdateConcurrencyException e)
+            catch (DbUpdateConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
             }
-           
+
         }
     }
 }
